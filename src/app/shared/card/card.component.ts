868130ed33c41ed685a514d01b6c3a90f6360e1f
Irshath <mohamedirshath.abdoulsamad@infotel.com>
2024-11-21 15:43:59 +0530

    added plus and minus button in mat card

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -6,6 +6,8 @@ import { Component, Input, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+
+  number: number = 0; // Initialize the number to start from 1
 @Input() srcLink !: string;
 @Input() price !:string | number;
 @Input() title !:string;
@@ -19,4 +21,18 @@ ngOnInit(){
   console.log(this.srcLink, this.price, this.title);
 
 }
+
+
+addnumber() {
+  this.number++; // Increment the number by 1
 }
+
+removenumber() {
+  if (this.number > 0) {
+    // Prevent the number from going below 0
+    this.number--;
+  }
+}
+
+}
+
